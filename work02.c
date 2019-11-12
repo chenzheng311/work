2668b44da717e0b0f40df95121e9bf594e8b0fbc
chenzheng311 <1686505293@qq.com>
2019-11-12 08:06:20 +0800

    qw frk;lmmk

--- work02.c
+++ work02.c
@@ -3,7 +3,35 @@ int main(void)
 {
     int x;
     printf("输入成绩\n");
-    scanf("%d",&x);
-    if(x>=90){printf("成绩为A\n");}else{if(x>=80){printf("成绩为B\n");}else{if(x>=70){printf("成绩为C\n");}else{if(x>=60){printf("成绩为D\n");}else{printf("成绩为E\n");}}}}}
+    scanf("%d", &x);
+    if (x >= 90)
+    {
+        printf("成绩为A\n");
+    }
+    else
+    {
+        if (x >= 80)
+        {
+            printf("成绩为B\n");
+        }
+        else
+        {
+            if (x >= 70)
+            {
+                printf("成绩为C\n");
+            }
+            else
+            {
+                if (x >= 60)
+                {
+                    printf("成绩为D\n");
+                }
+                else
+                {
+                    printf("成绩为E\n");
+                }
+            }
+        }
+    }
     return 0;
 }
