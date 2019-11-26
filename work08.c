2c5c56fc2984e72a6362358c6c6c32bfc3052ce9
chenzheng311 <1686505293@qq.com>
2019-11-26 08:16:06 +0800

    dasdqasd

--- work08.c
+++ work08.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-    int x,y,z;
-    for(x=0;x<=100;x++)
-    for(y=0;y<=100;y++)
-    for(z=0;z<=100;z++)
-    {
-        if(x*5+y*3+z/3==100&&z%3==0&&x+y+z==100)
-        printf("¹«¼¦=%d Ä¸¼¦=%d Ð¡¼¦=%d",x,y,z);
-    }
-        
-    
+    int x, y, z;
+    for (x = 0; x <= 100; x++)
+        for (y = 0; y <= 100; y++)
+            for (z = 0; z <= 100; z++)
+            {
+                if (x * 5 + y * 3 + z / 3 == 100 &&z % 3 == 0 && x + y + z == 100)
+                    printf("å…¬é¸¡=%d æ¯é¸¡=%d å°é¸¡=%d\n", x, y, z);
+            }
+
     return 0;
 }
